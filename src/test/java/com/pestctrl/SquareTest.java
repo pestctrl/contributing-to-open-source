91d275c1f1e957bd3bacdfc0aac41c9ccc00c4b3
Benson Chu <bensonchu457@gmail.com>
2021-05-01 17:50:09 -0500

    Our indispensable little bird

---
+++ src/test/java/com/pestctrl/SquareTest.java
@@ -0,0 +1,15 @@
+package com.pestctrl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareTest {
+
+    @Test
+    void CanaryTest() {
+        assert(true);
+    }
+}
